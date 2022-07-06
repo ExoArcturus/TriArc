52cedfba5bfd3ff1a5513d0b71ca578d00935ce2
Alastair Claringbold <108469924+ExoArcturus@users.noreply.github.com>
2022-07-06 13:45:37 +0100

    Add files via upload

---
+++ molecular_weights.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 17:00:25 2022
+
+@author: arcturus
+"""
+
+mass = {}
+mass['H2'] = 2
+mass['He'] = 4
+mass['CH4'] = 16
+mass['CO'] = 28
+mass['CO2'] = 44 
+mass['H2O'] = 18
+mass['H2S'] = 34
+mass['HCN'] = 27 
+mass['O2'] = 32 
+mass['O3'] = 48 
+mass['PH3'] = 34
+mass['NH3'] = 17
+mass['C2H2'] = 26 
+mass['N2'] =  28 
+mass['SO2'] = 64
+mass['NO'] =  30
+mass['HC3N'] = 51
+mass['CH2O'] = 30
